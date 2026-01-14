c00373c74a9baa0569a736e9b1a8eb852cd3fd39
SyntaxError <jonathan.lindh08@gmail.com>
2026-01-14 14:23:28 +0100

    problemlösning

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const l1 = document.getElementById("l1")
 const l2 = document.getElementById("l2")
 
+console.log("starting...")
 
 async function call_students_api(){
     try{
+        console.log("calling API")
         let response = await fetch("mockapi.ntig.dev/students/2000")
         let data = await response.jason
 
